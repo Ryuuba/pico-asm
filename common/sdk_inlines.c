cd2abcbcbe91ffe2405c35f620074f3b82bb1710
Adán G. Medrano-Chávez <ryuuba@gmail.com>
2024-04-05 08:17:59 -0600

    Add C wrappers

--- common/sdk_inlines.c
+++ common/sdk_inlines.c
@@ -5,10 +5,25 @@
  * that are marked as inline, ie. to be compiled in place.
  */
 
-void gpio_set_direction(int pin, int direction) {
+void gpio_set_dir_wrapper(int pin, int direction) {
     gpio_set_dir(pin, direction);
 }
 
-void gpio_set_state(int pin, int state) {
+void gpio_put_wrapper(int pin, int state) {
     gpio_put(pin, state);
+}
+
+void gpio_pull_up_wrapper(uint gpio) {
+    gpio_pull_up(gpio);
+}
+bool gpio_get_wrapper(uint gpio) {
+    return gpio_get(gpio);
+}
+
+void gpio_set_mask_wrapper(uint32_t mask) {
+    gpio_set_mask(mask);
+}
+
+void gpio_clr_mask_wrapper(uint32_t mask) {
+    gpio_clr_mask(mask);
 }
