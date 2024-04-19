d17292541fc368da01d2a7268102577b154217eb
Adán G. Medrano-Chávez <ryuuba@gmail.com>
2024-04-19 08:40:20 -0600

    Code rotary led behavior

---
+++ rotary_led/rotary_led.cc
@@ -0,0 +1,28 @@
+#include "rotary_led.h"
+#include "hardware/adc.h"
+
+
+
+void RotaryLed::init()
+{
+    // Initilizes digital output
+    for (auto it = pins.begin(); it < pins.end() - 1; it++)
+    {
+        gpio_init(*it);
+        gpio_set_dir(*it, GPIO_OUT);
+    }
+
+    // Initializes analog input
+    adc_init();
+    assert(pins.back() >= 26 || pins.back() <= 29);
+    adc_gpio_init(pins.back());
+    adc_select_input(0); // TODO: select the appropriate analog channel
+}
+
+void RotaryLed::turn_led_on()
+{
+    gpio_clr_mask(mask);
+    uint8_t pin = adc_read() % 7;
+    mask = 1 << pins[pin];
+    gpio_set_mask(mask);
+}
