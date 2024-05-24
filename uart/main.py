f31ab59eab3afead98a3c3bfe6e632fa11737b38
Adán G. Medrano-Chávez <ryuuba@gmail.com>
2024-05-24 13:05:13 -0600

    UART example works OK

--- uart/main.py
+++ uart/main.py
@@ -1,14 +1,41 @@
 from machine import Pin, UART, I2C
 from utime import sleep
+from ssd1306 import SSD1306_I2C
 
 def main() -> None:
     uart = UART(0, baudrate=9600, tx=Pin(12), rx=Pin(13))
+    i2c = I2C(0, sda=Pin(16), scl=Pin(17), freq=400000)
+    oled = SSD1306_I2C(128, 64, i2c)
+    oled.text('Receiver ON', 0, 0)
+    oled.show()
     while True:
         if uart.any():
-            msg = uart.read()
-            print(f'{msg} mundo')
-            uart.write('mundo')
-            sleep(1)
+            cmd = uart.read(1)
+            try:
+                cmd_val = int(cmd)
+            except ValueError:
+                print(cmd)
+                oled.text(f'Error: cmd', 0, 10)
+                oled.text('is not numeric', 0, 20)
+            except:
+                oled.text('Error: cmd unknown', 0, 10)
+            else:
+                print(cmd_val)
+                if cmd_val == 1:
+                    oled.fill(0)
+                    oled.text('Running cmd 1', 0, 10)
+                elif cmd_val == 2:
+                    oled.fill(0)
+                    oled.text('Running cmd 2', 0, 10)
+                elif cmd_val == 3:
+                    oled.fill(0)
+                    oled.text('Running cmd 3', 0, 10)
+                else:
+                    oled.fill(0)
+                    oled.text(f'Unknown cmd {cmd_val}', 0, 10)
+            finally:
+                oled.text('Receiver ON', 0, 0)
+                oled.show()
             
 
 if __name__ == '__main__':
--- uart/uart.c
+++ uart/uart.c
@@ -3,26 +3,36 @@
 
 #define UART_ID uart0
 #define BAUD_RATE 9600
+#define BUTTON_GPIO 15
+#define LED 16
 
 #include "pico/stdlib.h"
 #include "hardware/uart.h"
-#include <string.h>
-#include <stdio.h>
+#include "hardware/gpio.h"
 
 int main()
 {
+    uint8_t cmd = 0;
     uart_init(UART_ID, BAUD_RATE);
     gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
     gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
-    uint8_t buffer[5];
+
+    gpio_init(BUTTON_GPIO);
+    gpio_set_dir(BUTTON_GPIO, GPIO_IN);
+    gpio_pull_up(BUTTON_GPIO);
+
+    gpio_init(LED);
+    gpio_set_dir(LED, GPIO_OUT);
+    gpio_put(LED, 0);
     while (true)
     {
-        // sends a command to the receiver
-        uart_puts(UART_ID, "Hola");
-        // waits for response world
-        //TODO: Add interrupt to break the loop
-        uart_read_blocking(UART_ID, buffer, 5);
-        printf("Hola %s", buffer);
-        sleep_ms(1000);
+        if (!gpio_get(BUTTON_GPIO)) {
+            gpio_put(LED, 1);
+            // sends a command to the receiver
+            uart_putc(UART_ID, cmd+0x30);
+            cmd = (cmd + 1) & 3;
+            sleep_ms(1000);
+        }
+        gpio_put(LED, 0);
     }
 }
